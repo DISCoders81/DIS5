45adc6aff3fda1d27c5e3ccfaf9f296f33042c61
Joan T Pape <joanpape@gmx.de>
2014-05-26 02:45:25 +0200

    - Changed Logging to one File
    - Changed way of saving/logging to FileReader/Writer
    
    - Added Logging of Begin of Transaction and Commit actions
    
    - Added LoggingTypes to distinguish different Logging Actions
    
    - Added Crash Recovery functionality
    	(nach einem crash wird im log gesucht welche Transaktionen committed
    	waren. Dann werden die dazugehörigen Pages gesucht. Dann wird noch
    	verglichen ob die logSequenceNumbers der Pages und des Logs sich
    	unterscheiden und wenn die der Page kleiner ist -> also nicht aktuell,
    	wird die Write Operation wiederholt und die LSN der Page angeglichen)
    	
    - Added Main Menu to choose how many clients will be created and if the
    crash recovery should be run.
    
    - the current LSN is now written to and load from a file so that the
    number will always increase and can not occur twice.
    
    
    -> Aufgabe 3 fertig!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
 /**
  * @author joantomaspape Main class in which the application can be started
  */
@@ -6,32 +12,105 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Client client1 = new Client(1);
-		Client client2 = new Client(2);
-		Client client3 = new Client(3);
-		Client client4 = new Client(4);
-		Client client5 = new Client(5);
+		start();
 
-		client1.run();
-		client2.run();
-		client3.run();
-		client4.run();
-		client5.run();
+	}
+
+
+	private static void startClients(Client[] clients) {
+
+		for (int i = 0; i < clients.length; i++)
+		{
+			clients[i].run();
+		}
 
 		try
 		{
-			Thread.sleep(10000);
+			Thread.sleep(5000 + 1000 * clients.length);
 		} catch (InterruptedException e)
 		{
 			e.printStackTrace();
 		}
 
-		client1.interrupt();
-		client2.interrupt();
-		client3.interrupt();
-		client4.interrupt();
-		client5.interrupt();
+		for (int i = 0; i < clients.length; i++)
+		{
+			clients[i].interrupt();
+		}
+	}
+
+
+	private static void start() {
+
+		BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
+		int amountOfClients = 1;
+		boolean crashRecoveryEnabled = false;
+
+		System.out.print("This is the database builder. \n\nHow many clients do you want to create (min 1, max 10)?\n-> ");
+		try
+		{
+			amountOfClients = Integer.parseInt(inputReader.readLine()) % 10;
+
+			System.out.println("\nOk. " + amountOfClients + " Clients will be created.");
+
+			while (true)
+			{
+				System.out.print("\nDo you want to enable the crash recovery? \n(Yes/No) ->");
+				String enable = inputReader.readLine();
+
+				if (enable.equalsIgnoreCase("Yes"))
+				{
+					crashRecoveryEnabled = true;
+					break;
+				} else if (enable.equalsIgnoreCase("No"))
+				{
+					crashRecoveryEnabled = false;
+					break;
+				} else
+				{
+					System.err.println("Input incorrect. Try again..");
+					Thread.sleep(150);
+				}
+			}
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			try
+			{
+				inputReader.close();
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("\nThanks. Have fun!");
+		System.out.println("--------------------");
+		System.out.println("-------- DB --------");
+		System.out.println("--------------------");
+
+		if (crashRecoveryEnabled)
+		{
+			PersistanceManager.getInstance().crashRecovery();
+		}
+
+		System.out.println("--------------------");
+		System.out.println("- Creating Clients -");
+		System.out.println("---------...--------");
+
+		startClients(createClients(amountOfClients));
 
 	}
 
+
+	private static Client[] createClients(int n) {
+
+		ArrayList<Client> clientList = new ArrayList<Client>();
+		for (int i = 0; i < n; i++)
+		{
+			clientList.add(new Client(i));
+		}
+		return clientList.toArray(new Client[clientList.size()]);
+	}
 }
--- src/PersistanceManager.java
+++ src/PersistanceManager.java
@@ -1,8 +1,10 @@
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.Properties;
 
 
 /**
@@ -21,6 +23,36 @@ public class PersistanceManager {
 
 	private PersistanceManager() {
 
+		BufferedReader reader = null;
+		try
+		{
+			reader = new BufferedReader(new FileReader("logSequenceNumber"));
+			this.logSequenceNumber = Integer.parseInt(reader.readLine());
+		} catch (FileNotFoundException e)
+		{
+			this.logSequenceNumber = 0;
+		} catch (NumberFormatException e)
+		{
+			e.printStackTrace();
+			return;
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			return;
+		} finally
+		{
+			if (reader != null)
+			{
+				try
+				{
+					reader.close();
+				} catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 
@@ -36,10 +68,40 @@ public class PersistanceManager {
 	}
 
 
+	private synchronized void incrementLSN() {
+
+		FileWriter writer = null;
+
+		try
+		{
+			writer = new FileWriter("logSequenceNumber");
+			writer.write(Integer.toString(logSequenceNumber + 1));
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			if (writer != null)
+			{
+				try
+				{
+					writer.close();
+				} catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+		this.logSequenceNumber++;
+	}
+
+
 	public Transaction beginTransaction() {
 
 		incrementTaId();
-		System.out.println("Begin of Transaction Nr: " + currentTransactionId);
+
+		log(currentTransactionId, LogType.BOT);
 		return new Transaction(currentTransactionId);
 	}
 
@@ -47,21 +109,21 @@ public class PersistanceManager {
 	public synchronized void commit(Transaction ta) {
 
 		// commit the given transaction
-		System.out.println("Transaction Nr: " + currentTransactionId
-				+ "was committed!");
+
+		log(ta.getTaId(), LogType.COMMIT);
 		ta.commit();
 	}
 
 
 	public synchronized void write(Transaction ta, int pageId, String data) {
 
-		this.logSequenceNumber++;
+		incrementLSN();
 
 		// write the data to a file
 		// one file for each page
 		// LSN also into the file for redoing
 
-		log(ta.getTaId(), pageId, data);
+		log(ta.getTaId(), LogType.WRITE, pageId, data);
 
 		Page page = new Page(pageId, logSequenceNumber, data, ta);
 		buffer.put(pageId, page);
@@ -87,20 +149,17 @@ public class PersistanceManager {
 
 	private synchronized void savePage(Page page) {
 
-		Properties prop = new Properties();
-		OutputStream output = null;
+		FileWriter writer = null;
 
 		try
 		{
-			output = new FileOutputStream("page" + page.getPageId());
-			prop.setProperty("LSN", Integer.toString(page.getLsn()));
-			prop.setProperty("taId", Integer.toString(page.getTa().getTaId()));
-			prop.setProperty("data", page.getData());
-			prop.store(output, "Page_Data");
-			System.out.println("Stored: " + "\nPageNr " + page.getPageId()
-					+ "\nLSN " + page.getLsn() + "\ntaId "
-					+ page.getTa().getTaId() + "\nWith Data: " + page.getData()
-					+ "\n--------------------");
+			writer = new FileWriter("page" + page.getPageId());
+			writer.write("LSN:" + Integer.toString(page.getLsn()) + "|");
+			writer.write("taId:" + Integer.toString(page.getTa().getTaId()) + "|");
+
+			writer.write("data:" + page.getData());
+
+			System.out.println("Stored: " + "\nPageNr " + page.getPageId() + "\nLSN " + page.getLsn() + "\ntaId " + page.getTa().getTaId() + "\nWith Data: " + page.getData() + "\n--------------------");
 		} catch (IOException e)
 		{
 			e.printStackTrace();
@@ -108,9 +167,9 @@ public class PersistanceManager {
 		{
 			try
 			{
-				if (output != null)
+				if (writer != null)
 				{
-					output.close();
+					writer.close();
 				}
 			} catch (IOException ex)
 			{
@@ -120,21 +179,46 @@ public class PersistanceManager {
 	}
 
 
-	private synchronized void log(int taId, int pageId, String data) {
+	/**
+	 * This method logs all writing actions. There is another log method used
+	 * for BOT and Write. Both methods are the same apart from the given
+	 * parameters.
+	 * 
+	 * @param taId
+	 * @param type
+	 * @param pageId
+	 * @param data
+	 */
+	private synchronized void log(int taId, LogType type, int pageId, String data) {
 
-		Properties prop = new Properties();
-		OutputStream output = null;
+		FileWriter writer = null;
 
 		try
 		{
-			output = new FileOutputStream("log_page" + pageId);
-			prop.setProperty("LSN", Integer.toString(logSequenceNumber));
-			prop.setProperty("taId", Integer.toString(taId));
-			prop.setProperty("data", data);
-			prop.store(output, "Log Eintrag");
-			System.out.println("Logged: " + "\nPageNr " + pageId + "\nLSN "
-					+ logSequenceNumber + "\ntaId " + taId + "\nWith Data: "
-					+ data + "\n--------------------");
+
+			writer = new FileWriter("TransactionLog", true);
+			writer.write("LSN:" + Integer.toString(logSequenceNumber) + "|");
+			writer.write("taId:" + Integer.toString(taId) + "|");
+
+			switch (type) {
+				case COMMIT: {
+					System.err.println("This method is not used to log committing operations");
+					return;
+				}
+				case BOT: {
+					System.err.println("This method is not used to log begin of transaction operations");
+					return;
+				}
+				case WRITE: {
+					writer.write("data:" + data + "|");
+					writer.write("pageId:" + pageId);
+					break;
+				}
+			}
+
+			writer.write("\n");
+			System.out.println("LOGGED: " + "\nPageNr " + pageId + "\nLSN " + logSequenceNumber + "\ntaId " + taId + "\nWith Data: " + data + "\n--------------------");
+
 		} catch (IOException e)
 		{
 			e.printStackTrace();
@@ -142,14 +226,286 @@ public class PersistanceManager {
 		{
 			try
 			{
-				if (output != null)
+				if (writer != null)
 				{
-					output.close();
+					writer.close();
 				}
 			} catch (IOException ex)
 			{
 				ex.printStackTrace();
 			}
 		}
+
+	}
+
+
+	/**
+	 * This method is used for the logging of BOT and Commit actions. There is
+	 * another log method for Writing actions which is the same apart from the
+	 * given parameters.
+	 * 
+	 * @param taId
+	 * @param type
+	 */
+	private synchronized void log(int taId, LogType type) {
+
+		FileWriter writer = null;
+
+		try
+		{
+
+			writer = new FileWriter("TransactionLog", true);
+			writer.write("LSN:" + Integer.toString(logSequenceNumber) + "|");
+			writer.write("taId:" + Integer.toString(taId) + "|");
+
+			switch (type) {
+				case COMMIT: {
+					writer.write("data:committed");
+					System.out.println("LOGGED: " + "\nLSN " + logSequenceNumber + "\ntaId " + taId + "\nCOMMITTED\n--------------------");
+					break;
+				}
+				case BOT: {
+					writer.write("data:BOT");
+					System.out.println("LOGGED: " + "\nLSN " + logSequenceNumber + "\ntaId " + taId + "\nBEGIN OF TRANSACTION \n--------------------");
+					break;
+				}
+				case WRITE: {
+					// This is not used for write operations
+					System.err.println("This method is not used to log writing operations");
+					return;
+				}
+			}
+
+			writer.write("\n");
+
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			try
+			{
+				if (writer != null)
+				{
+					writer.close();
+				}
+			} catch (IOException ex)
+			{
+				ex.printStackTrace();
+			}
+		}
+
+	}
+
+
+	public void crashRecovery() {
+
+		Integer[] winnerTAs = determineWinnerTaIds();
+		if (winnerTAs != null)
+		{
+			redoTransactions(winnerTAs);
+		}
+	}
+
+
+	/**
+	 * Reads all of the logs in order to determine which of the logged
+	 * transactions have been committed
+	 * 
+	 * @return all of those transaction's ids
+	 */
+	private Integer[] determineWinnerTaIds() {
+
+		BufferedReader reader = null;
+		ArrayList<Integer> winnerTas = new ArrayList<Integer>();
+
+		try
+		{
+			reader = new BufferedReader(new FileReader("TransactionLog"));
+			String line = reader.readLine();
+			String[] values;
+
+			while (line != null)
+			{
+
+				values = line.split("\\|");
+
+				switch (values[2].split("\\:")[1]) {
+					case "committed": {
+						winnerTas.add(Integer.parseInt(values[1].split("\\:")[1]));
+						break;
+					}
+					case "BOT": {
+						// BOT
+						break;
+					}
+					default: {
+						// data
+						break;
+					}
+				}
+
+				line = reader.readLine();
+
+			}
+
+		} catch (FileNotFoundException e)
+		{
+			System.err.println("No TransactionLog File found. Crash Recovery is impossible\nSkipping Crash Recovery..");
+			return null;
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			try
+			{
+				if (reader != null)
+				{
+					reader.close();
+				}
+			} catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+
+		}
+		return (Integer[]) winnerTas.toArray(new Integer[winnerTas.size()]);
+
+	}
+
+
+	private void redoTransactions(Integer[] taIds) {
+
+		BufferedReader reader = null;
+
+		try
+		{
+			reader = new BufferedReader(new FileReader("TransactionLog"));
+			String line = reader.readLine();
+			String[] values;
+
+			while (line != null)
+			{
+				values = line.split("\\|");
+
+				// Go through all Log entries
+				if (values.length != 3)
+				{
+					for (int currentTaId : taIds)
+					{
+						// find those that are write operations belonging to the
+						// winner transactions
+						if (currentTaId == Integer.parseInt(values[1].split("\\:")[1]))
+						{
+							// compare lsn of the write operation to lsn of the
+							// page
+							if (!compareLSNToPageLSN(Integer.parseInt(values[0].split("\\:")[1]), Integer.parseInt(values[3].split("\\:")[1])))
+							{
+								// the operation has to be redone
+								redoWrite(values);
+							}
+						}
+					}
+				}
+
+				line = reader.readLine();
+			}
+
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			try
+			{
+				if (reader != null)
+				{
+					reader.close();
+				}
+			} catch (Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		}
+	}
+
+
+	private void redoWrite(String[] values) {
+
+		FileWriter writer = null;
+		String newLSN = values[0].split("\\:")[1];
+		String ta = values[1].split("\\:")[1];
+		String data = values[2].split("\\:")[1];
+		String pageId = values[3].split("\\:")[1];
+
+		try
+		{
+			writer = new FileWriter("page" + pageId);
+
+			writer.write("LSN:" + newLSN + "|");
+			writer.write("taId:" + ta + "|");
+			writer.write("data:" + data);
+
+			System.out.println("Redone!: " + "\nPageNr " + pageId + "\nLSN " + newLSN + "\ntaId " + ta + "\nWith Data: " + data + "\n--------------------");
+
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			try
+			{
+				writer.close();
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+
+	/**
+	 * returns true if LSN is smaller than PageLsn -> in this case the write
+	 * action does not have to be redone; And returns false in case the
+	 * operation has to be redone;
+	 * 
+	 * @param lsn
+	 * @param pageId
+	 * @return
+	 */
+	private boolean compareLSNToPageLSN(int lsn, int pageId) {
+
+		BufferedReader reader = null;
+
+		String[] line;
+		try
+		{
+			reader = new BufferedReader(new FileReader("page" + pageId));
+			line = reader.readLine().split("\\|");
+		} catch (FileNotFoundException e)
+		{
+			System.err.println("File >>page" + pageId + ">> does not exist and will be created..");
+			return false;
+		} catch (Exception e)
+		{
+			System.err.println("File >>page" + pageId + ">> is corrupted!");
+			return false;
+		} finally
+		{
+			try
+			{
+				if (reader != null)
+				{
+					reader.close();
+				}
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
+		return (Integer.parseInt(line[0].split("\\:")[1]) < lsn);
 	}
 }
